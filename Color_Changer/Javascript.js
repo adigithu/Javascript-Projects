da2bef90155c63f137b5dfed83ccae4f6a437641
Aditya J Parida <adityaparida605@gmail.com>
2026-06-18 11:18:43 +0530

    Color Changer

--- Color_Changer/Javascript.js
+++ Color_Changer/Javascript.js
@@ -5,20 +5,20 @@ buttons.forEach(function (button){
     console.log(button)
     button.addEventListener('click', function (event){
         console.log(event)
-        console.log(event.target) //From where the event is coming
-        if (event.target==='grey'){
+        console.log(event.target) //From where the event is coming and it is the entire HTML document
+        if (event.target.id==='grey'){
             body.style.backgroundColor=event.target.id;
         }
-        if (event.target==='white'){
+        if (event.target.id==='white'){
             body.style.backgroundColor=event.target.id;
         }
-        if (event.target==='blue'){
+        if (event.target.id==='blue'){
             body.style.backgroundColor=event.target.id;
         }
-        if (event.target==='yellow'){
+        if (event.target.id==='yellow'){
             body.style.backgroundColor=event.target.id;
         }
-        if (event.target==='purple'){
+        if (event.target.id==='purple'){
             body.style.backgroundColor=event.target.id;
         }
     });
